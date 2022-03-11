ed356bbdf729d77a3a3f7a6a411fe8ee051978a7
christopher james <kushyapp916@gmail.com>
2022-03-11 09:37:22 -0800

    added git hub

--- index.php
+++ index.php
@@ -1,9 +1,18 @@
-<p>&nbsp;</p>
-<h1 style="text-align: center;">9 O N E 6 . N E T</h1>
-<h3 style="text-align: center;">DEMOS</h3>
-<p>&nbsp;</p>
-<p style="text-align: center;"><a href="../../endo">Endo</a></p>
-<p style="text-align: center;"><a href="../../videos">Videos</a></p>
-<p style="text-align: center;"><a href="../../crm">CRM</a></p>
-<p style="text-align: center;"><a href="../../analytics">Analytics</a></p>
-<p style="text-align: center;"><a href="../../multi-shop">Multi-Shop</a></p>
+<head>
+  <title>Font Awesome Icons</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
+<body>
+  <p>&nbsp;</p>
+  <h1 style="text-align: center;">9 O N E 6 . N E T</h1>
+  <h3 style="text-align: center;">DEMOS</h3>
+  <p>&nbsp;</p>
+  <p style="text-align: center;"><a href="../../endo">Endo</a></p>
+  <p style="text-align: center;"><a href="../../videos">Videos</a></p>
+  <p style="text-align: center;"><a href="../../crm">CRM</a></p>
+  <p style="text-align: center;"><a href="../../analytics">Analytics</a></p>
+  <p style="text-align: center;"><a href="../../multi-shop">Multi-Shop</a></p>
+  <p style="text-align: center;"><a href="https://github.com/chris-jk"><i style="font-size:24px" class="fa">&#xf09b;</i></a></p>
+</body>
