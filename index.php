52442e914f81ab64e7deaa78aa2a032cef4a2244
christopher james <kushyapp916@gmail.com>
2022-03-11 09:35:56 -0800

    landing page

---
+++ index.php
@@ -0,0 +1,9 @@
+<p>&nbsp;</p>
+<h1 style="text-align: center;">9 O N E 6 . N E T</h1>
+<h3 style="text-align: center;">DEMOS</h3>
+<p>&nbsp;</p>
+<p style="text-align: center;"><a href="../../endo">Endo</a></p>
+<p style="text-align: center;"><a href="../../videos">Videos</a></p>
+<p style="text-align: center;"><a href="../../crm">CRM</a></p>
+<p style="text-align: center;"><a href="../../analytics">Analytics</a></p>
+<p style="text-align: center;"><a href="../../multi-shop">Multi-Shop</a></p>
