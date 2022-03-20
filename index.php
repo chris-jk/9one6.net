213bd77ba2f0ae685e3fdbeb059ac77931c3960a
Chris James <58401647+chris-jk@users.noreply.github.com>
2022-03-20 13:35:42 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,18 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
 <head>
-  <title>Font Awesome Icons</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <title>9 O N E 6 . N E T</title>
 </head>
-
 <body>
   <p>&nbsp;</p>
   <h1 style="text-align: center;">9 O N E 6 . N E T</h1>
   <h3 style="text-align: center;">DEMOS</h3>
   <p>&nbsp;</p>
-  <p style="text-align: center;"><a href="../../endo">Endo</a></p>
-  <p style="text-align: center;"><a href="../../videos">Videos</a></p>
   <p style="text-align: center;"><a href="../../crm">CRM</a></p>
   <p style="text-align: center;"><a href="../../analytics">Analytics</a></p>
+  <p style="text-align: center;"><a href="../../videos">Videos</a></p>
   <p style="text-align: center;"><a href="../../multi-shop">Multi-Shop</a></p>
+  <p style="text-align: center;"><a href="../../endo">Endo</a></p>
+  <p style="text-align: center;"><a href="../../strain-guide">StrainGuide</a></p>
+  <p style="text-align: center;"><a href="../../">you-stagram</a></p>
   <p style="text-align: center;"><a href="https://github.com/chris-jk"><i style="font-size:24px" class="fa">&#xf09b;</i></a></p>
 </body>
+</body>
+</html>
